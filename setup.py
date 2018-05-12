37663873f8a0739212004e34d681a2f30f09e6bc
Andrew Shay <a>
2018-05-12 00:07:14 -0400

    v bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lektor-read-full-post',
-    version='0.1',
+    version='0.2',
     author='Andrew Shay',
     url='https://andrewshay.me',
     license='MIT',
