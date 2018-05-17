b91eb16857bde66fafca805d17a34e93d440b9ce
Andrew Shay <a>
2018-05-16 21:03:21 -0400

    added long desc

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='lektor-read-full-post',
-    version='0.2',
+    version='0.3',
     author='Andrew Shay',
     url='https://andrewshay.me',
+    long_description="Lektor plugin that allows blog listing posts to be shortened with a link to the full post.",
     license='MIT',
     py_modules=['lektor_read_full_post'],
     entry_points={
